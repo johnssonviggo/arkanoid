148fb8ad024488663a757086bd932345913d2ae1
johnssonviggo <viggo.johnsson1@gmail.com>
2024-10-07 09:50:11 +0200

    paddle movement

--- arkanoid.rb
+++ arkanoid.rb
@@ -2,7 +2,7 @@ require 'gosu'
 
 class Ball
 
-  attr_reader :x, :y
+  attr_reader :x, :y, :width, :height, :vel_y, :vel_x
 
   def initialize()
     @image = Gosu::Image.new("./img/item__66.png")
@@ -10,13 +10,15 @@ class Ball
     @y = 500
     @vel_x = [-3, 3].sample
     @vel_y = [-3, 3].sample
+    @width = 53
+    @height = 53
   end
 
   def update
-    if @x >= 1200 - 53 || @x <= 0
+    if @x >= 1200 - @width || @x <= 0
       @vel_x *= -1
     end
-    if @y >= 900 - 100 || @y <= 0
+    if @y <= 0
       @vel_y *= -1
     end
     @x += @vel_x
@@ -44,13 +46,16 @@ class Bounce < Gosu::Window
   end
 
   def update
+    @player.move_left if button_down?(Gosu::KB_LEFT)
+    @player.move_right if button_down?(Gosu::KB_RIGHT)
+
     @balls.each {|ball| ball.update}
 
     @balls.each do |ball|
       ball.update
-      if @player.hit_by?(ball)  # Om bollen träffar paddeln
-        ball.bounce_off_paddle
-      end
+       if @player.hit_by?(ball)  # Om bollen träffar paddeln
+         ball.bounce_off_paddle
+       end
     end
   end
 
@@ -69,24 +74,42 @@ class Bounce < Gosu::Window
 
 
 end
- h
+
 class Paddle
+  @direction = nil
   WIDTH = 100
+  SPEED = 10
+  HEIGHT = 25
   def initialize()
     @x1 = 550
     @y1 = 750
   end
 
-  def draw
-    Gosu::draw_rect(@x1, @y1, WIDTH, 25, Gosu::Color:: WHITE, 1)
+  def move_left
+    @x1 -= SPEED unless @x1 <= 0  # Begränsar paddeln så att den inte går utanför vänster sida
+  end
+
+  def move_right
+    @x1 += SPEED unless @x1 + WIDTH >= 1200  # Begränsar paddeln så att den inte går utanför höger sida
   end
 
   def hit_by?(ball)
     # Kontrollera om bollen överlappar med paddelns rektangel
-    ball.x > @x1 && ball.x < @x1 + WIDTH &&
-    ball.y + 53 > @y1 && ball.y < @y1 + HEIGHT
+    # ball.x > @x1 && ball.x < @x1 + WIDTH &&
+    # ball.y + 53 > @y1 && ball.y < @y1 + HEIGHT
+
+    if ball.y + ball.height >= @y1 && ball.y + ball.height <= @y1 + HEIGHT &&
+      ball.x + ball.width > @x1 && ball.x < @x1 + WIDTH && ball.vel_y > 0
+     true
+   else
+     false
+   end
   end
 
+
+  def draw
+    Gosu::draw_rect(@x1, @y1, WIDTH, HEIGHT, Gosu::Color:: WHITE, 1)
+  end
 
 end
 
